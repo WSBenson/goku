f312bb3de552d21493f43d10428455d7db515540
Willem Benson <willembenson@Willems-MacBook-Pro.local>
2020-05-27 19:25:51 -0400

    initial commit before power levels

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # goku-http-server
-go http server in docker container
+
+This is my Goku http server written in go and containerized using docker.
+
+The purpose of this server is to take a JSON post request and return a response ranking each character by their power level.
+
+At the moment it will simply add "Hello, " + the name in the JSON body
+
+For Example:
+
+The JSON file for the POST request should look something like this:
+
+```json
+{
+	"name": "Goku"
+}
+```
+
+Which will return a response of:
+
+```json
+{
+    "Response": "Hello, Goku"
+}
+```
+This will be changed in the future once the program is ready to rank characters by power levels.
+
+## Gettting Started
+
+### Building
+
+To Build this project follow these steps:
+- Make sure you have docker installed
+- Clone my Git repository where ever you want
+- Then change directory to wsbenson/goku-http-server
+- Run these two commands back to back in bash:
+    ```bash
+       docker build -t goku .
+
+       docker run -p 3000:3000 goku
+    ```
+- If port 3000 is occupied for you, change the PORT env variable in the Dockerfile
+- Then make sure to update the second command with your new port
+
+- The http server should now be running, you can check by going to:
+    `http://localhost:3000/test`   <- Change the port number to match yours
+
+
+
+### Testing
+
+- Send a JSON POST request to: `http://localhost:3000/test` or whatever port you used
+- You should get a response similar to the one above the "Getting Started" section.
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// struct to hold the name field from the JSON request body
+type testRequest struct {
+	Name string `json:"name"`
+}
+
+// struct to hold the new concatination of the user's name
+type testResponse struct {
+	Response string
+}
+
+func main() {
+
+	// Bind the viper key "prt" to the env variable "PORT" from the
+	// dockerfile then get the value from the "PORT" env variable
+	viper.BindEnv("prt", "PORT")
+	prt := viper.Get("prt")
+
+	// Convert "PORT" env variable to a string
+	str := fmt.Sprintf("%v", prt)
+
+	// concats the address with the port number from the env variable
+	address := "0.0.0.0:" + str
+
+	// creates mux which is the router used for the http server
+	mux := http.NewServeMux()
+	// sets the endpoint /test to run the function testHandler
+	mux.HandleFunc("/test", testHandler)
+
+	// set the fields of the http server, the Handler will point to
+	// the router
+	goSrvr := http.Server{
+		Addr:         address,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// start the http server, it will stop execution if an error is returned
+	panic(goSrvr.ListenAndServe())
+
+}
+
+// testHandler accepts a post request that takes the user's name from the JSON request
+// and responds back with "Hello" + that user's name
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		// Reads the body of the JSON request into the testRequest struct
+		// by unmarshaling the JSON bytes
+		tr := testRequest{}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.Unmarshal(body, &tr)
+
+		// Concatinates the name from the POST body with "Hello"
+		// then marshals the response back into JSON bytes to
+		// send to the browser.
+		tres := testResponse{
+			Response: "Hello, " + tr.Name,
+		}
+		b, err := json.Marshal(tres)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Writes the JSON bytes to the JSON response writer
+		w.Write(b)
+
+	case http.MethodGet:
+		fmt.Fprintln(w, "Get not supported")
+	}
+
+}
