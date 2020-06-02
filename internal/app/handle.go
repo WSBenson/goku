3183abca3a01a0a489edd937f83aa8f064dfd5ec
Willem Benson <willemb17@gmail.com>
2020-06-02 16:35:03 -0400

    added changes for first powerlevel PR

--- internal/app/handle.go
+++ internal/app/handle.go
@@ -3,7 +3,6 @@ package app
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -12,9 +11,7 @@ import (
 )
 
 var (
-	logger = zerolog.New(
-		zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339},
-	).With().Timestamp().Caller().Logger()
+	logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).With().Timestamp().Caller().Logger()
 )
 
 type fighters struct {
@@ -36,13 +33,16 @@ type allegiance struct {
 func handleGokuRequests(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
+		logger.Info().Msg("Goku POST request received")
 		handleGokuPost(w, r)
 
 	case http.MethodGet:
+		logger.Info().Msg("Goku GET request received")
 		handleGokuGet(w, r)
 
 	// all other cases
 	default:
+		logger.Info().Msg("Goku other request received")
 		handleGokuDefault(w, r)
 	}
 
@@ -52,38 +52,49 @@ func handleGokuPost(w http.ResponseWriter, r *http.Request) {
 	// Reads the body of the JSON request into the fighter struct
 	// by unmarshaling the JSON bytes
 	f := fighters{}
+	logger.Info().Msg("Reading POST request...")
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to read POST request body.")
 	}
-	json.Unmarshal(body, &f)
+	logger.Info().Msg("Unmarshaling POST request...")
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to unmarshal POST request body.")
+	}
 
-	// Concatinates the name from the POST body with a string
-	// then marshals the response back into JSON bytes to
-	// send to the browser.
+	// The struct that will hold the JSON message that is sent back to the browser
 	a := allegiance{}
-	switch length := len(f.Fighters); {
-	case length == 1:
-		lenOfOne(f.Fighters[0], &a)
 
-	case length == 2:
-		lenOfTwo(f.Fighters[0], f.Fighters[1], &a)
-
-	case length > 2:
-		lenOverTwo(f, &a)
-
-	default:
-		logger.Error().Msg("Improperly formated JSON message")
-		a.Message = "You did not format your JSON message properly, check the console log and the README."
+	// Checks for any JSON name and power fields that are empty, or zero.
+	// (Not that important so this could be removed, I just prefer names to not be "" and powers to not be 0)
+	for _, fighter := range f.Fighters {
+		if fighter.Name == "" {
+			logger.Error().Msg("Some fighter name JSON fields are empty.\n")
+			a.Message = "Some fighter name JSON fields are empty. Check the console log and the README."
+			return
+		}
+		if fighter.Power == 0 {
+			logger.Error().Msg("Some fighter power JSON fields are 0.\n")
+			a.Message = "Some fighter power JSON fields are 0. Check the console log and the README."
+			return
+		}
 	}
 
+	// The gokuPOSTCases function evaluates how many fighters are in the JSON POST body
+	// and returns a concatinated string with the name of the fighter(s) and other information
+	a.Message = gokuPOSTCases(f)
+
+	// The response is marshaled back into JSON bytes to be sent to the browser
+	logger.Info().Msg("Marshaling POST message...")
 	d, err := json.Marshal(a)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to marshal POST message.")
 	}
 
 	// Writes the JSON bytes to the JSON response writer
 	w.Write(d)
+	logger.Info().Msg("POST message written\n")
 }
 
 func handleGokuGet(w http.ResponseWriter, r *http.Request) {
@@ -91,13 +102,15 @@ func handleGokuGet(w http.ResponseWriter, r *http.Request) {
 	a := allegiance{
 		Message: "The Z fighters are ready to battle",
 	}
+	logger.Info().Msg("Marshaling GET message...")
 	d, err := json.Marshal(a)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to marshal GET message.")
 	}
 
 	// Writes the JSON bytes to the JSON response writer
 	w.Write(d)
+	logger.Info().Msg("GET message written\n")
 }
 
 func handleGokuDefault(w http.ResponseWriter, r *http.Request) {
@@ -105,85 +118,13 @@ func handleGokuDefault(w http.ResponseWriter, r *http.Request) {
 	a := allegiance{
 		Message: "The Z fighters perished",
 	}
+	logger.Info().Msg("Marshaling message...")
 	d, err := json.Marshal(a)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to marshal message.")
 	}
 
 	// Writes the JSON bytes to the JSON response writer
 	w.Write(d)
-}
-
-// lenOfOne handles what JSON message to give back for a JSON array
-// with one fighter in it
-func lenOfOne(f fighter, a *allegiance) {
-	if f.Name == "" {
-		logger.Error().Msg("Some fighter name JSON fields not specified, or empty.")
-		a.Message = "Some fighter name JSON fields not specified, or empty. Check the console log and the README."
-		return
-	}
-	if f.Power == 0 {
-		logger.Error().Msg("Fighter power JSON field not specified, or 0.")
-		a.Message = "Fighter power JSON field not specified, or 0. Check the console log and the README."
-		return
-	} else if f.Power > 9000 {
-		a.Message = "The scouter says " + f.Name + "'s power level is over 9000! You better start running."
-	} else if f.Power < 206 {
-		a.Message = f.Name + " is weaker than Krillin, cmon bruh."
-	} else {
-		a.Message = f.Name + "'s power level isn't even over 9000, they're a straight side character."
-	}
-}
-
-// lenOfTwo handles what JSON message to give back for a JSON array
-// with two fighters in it
-func lenOfTwo(f fighter, f1 fighter, a *allegiance) {
-	if f.Name == "" || f1.Name == "" {
-		logger.Error().Msg("Some fighter name JSON fields not specified, or empty.")
-		a.Message = "Some fighter name JSON fields not specified, or empty. Check the console log and the README."
-		return
-	}
-	if f.Power == 0 || f1.Power == 0 {
-		logger.Error().Msg("Some fighter power JSON fields not specified, or 0.")
-		a.Message = "Some fighter power JSON fields not specified, or 0. Check the console log and the README."
-		return
-	}
-	if f.Power == f1.Power && f.Power < 206 && f1.Power < 206 {
-		a.Message = f.Name + " and " + f1.Name + " are equally trash, they better fuse or something."
-	} else if f.Power < 206 && f1.Power < 206 {
-		a.Message = f.Name + " and " + f1.Name + " are both weaker than Krillin, im done."
-	} else if f.Power > f1.Power {
-		a.Message = f.Name + "'s power level is superior to " + f1.Name + "'s"
-	} else if f.Power == f1.Power {
-		a.Message = f.Name + "'s power level is equal to " + f1.Name + "'s"
-	} else {
-		a.Message = f1.Name + "'s power level is superior to " + f.Name + "'s"
-	}
-}
-
-//lenOverTwo handles what JSON message to give back for a JSON array
-// with more than two fighters in it
-func lenOverTwo(fs fighters, a *allegiance) {
-	maxPower := 0
-	maxFighter := fs.Fighters[0]
-	// loops through each fighter to compare their powers
-	for _, fighter := range fs.Fighters {
-		if fighter.Name == "" {
-			logger.Error().Msg("Some fighter name JSON fields not specified, or empty.")
-			a.Message = "Some fighter name JSON fields not specified, or empty. Check the console log and the README."
-			return
-		}
-		if fighter.Power == 0 {
-			logger.Error().Msg("Some fighter power JSON fields not specified, or 0.")
-			a.Message = "Some fighter power JSON fields not specified, or 0. Check the console log and the README."
-			return
-		}
-		if fighter.Power > maxPower {
-			// Sets the highest power and the fighter who has the highest power
-			maxPower = fighter.Power
-			maxFighter = fighter
-		}
-	}
-
-	a.Message = maxFighter.Name + " is the strongest of all the Z fighters."
+	logger.Info().Msg("Message written\n")
 }
---
+++ internal/app/logic.go
@@ -0,0 +1,66 @@
+package app
+
+// The gokuPOSTCases function handles each case for the number of fighters passed
+// to the server through a JSON POST request
+func gokuPOSTCases(fs fighters) string {
+
+	switch length := len(fs.Fighters); {
+	case length == 1:
+		return evaluatePowerLvl(fs.Fighters[0])
+
+	case length == 2:
+		return compareTwoPowers(fs.Fighters[0], fs.Fighters[1])
+
+	case length > 2:
+		return compareOverTwoPowers(fs)
+
+	default:
+		logger.Error().Msg("Improperly formated JSON message")
+		return "You did not format your JSON message properly, check the console log and the README."
+	}
+}
+
+// The evaluatePowerLvl function handles the case where one fighter is passed to the server.
+// It will return a string that evaluates whether this fighter's power level is over 9000
+func evaluatePowerLvl(f fighter) string {
+	if f.Power > 9000 {
+		return "The scouter says " + f.Name + "'s power level is over 9000! You better start running."
+	} else if f.Power < 206 {
+		return f.Name + " is weaker than Krillin, cmon bruh."
+	} else {
+		return f.Name + "'s power level isn't even over 9000, they're a straight side character."
+	}
+}
+
+// The compareTwoPowers funcion handles the case where two fighters are passed to the server.
+// It will return a string that compares these two fighters' power levels (who is stronger).
+func compareTwoPowers(f fighter, f1 fighter) string {
+	if f.Power == f1.Power && f.Power < 206 && f1.Power < 206 {
+		return f.Name + " and " + f1.Name + " are equally trash, they better fuse or something."
+	} else if f.Power < 206 && f1.Power < 206 {
+		return f.Name + " and " + f1.Name + " are both weaker than Krillin, im done."
+	} else if f.Power > f1.Power {
+		return f.Name + "'s power level is superior to " + f1.Name + "'s"
+	} else if f.Power == f1.Power {
+		return f.Name + "'s power level is equal to " + f1.Name + "'s"
+	} else {
+		return f1.Name + "'s power level is superior to " + f.Name + "'s"
+	}
+}
+
+// The compareOverTwoPowers functions handles the case where more than two fighters are
+// passed to the server. It will return a string stating who has the highest power level.
+func compareOverTwoPowers(fs fighters) string {
+	maxPower := 0
+	maxFighter := fs.Fighters[0]
+	// loops through each fighter to compare their powers
+	for _, fighter := range fs.Fighters {
+		if fighter.Power > maxPower {
+			// Sets the highest power and the fighter who has the highest power
+			maxPower = fighter.Power
+			maxFighter = fighter
+		}
+	}
+
+	return maxFighter.Name + " is the strongest of all the Z fighters."
+}
