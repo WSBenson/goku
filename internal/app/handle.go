b12c1e9f201efb1c5f8dcd92f97c81e3cb649662
WSBenson <51492428+WSBenson@users.noreply.github.com>
2020-06-03 10:44:39 -0400

    completed power level output and added error messages with zerolog (#17)
    * completed power level output and added error messages with zerolog
    
    * added changes for first powerlevel PR

--- internal/app/handle.go
+++ internal/app/handle.go
@@ -3,12 +3,24 @@ package app
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
 )
 
+var (
+	logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).With().Timestamp().Caller().Logger()
+)
+
+type fighters struct {
+	Fighters []fighter `json:"fighters"`
+}
+
 type fighter struct {
-	Name string `json:"name"`
+	Name  string `json:"name"`
+	Power int    `json:"power"`
 }
 
 // struct to hold the new concatination of the user's name
@@ -16,18 +28,21 @@ type allegiance struct {
 	Message string `json:"message"`
 }
 
-// gokuHandler accepts a post request that takes the user's name from the JSON request
+// handleGokuRequests accepts a post request that takes the user's name from the JSON request
 // and responds back with that user's name + some message
 func handleGokuRequests(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
+		logger.Info().Msg("Goku POST request received")
 		handleGokuPost(w, r)
 
 	case http.MethodGet:
+		logger.Info().Msg("Goku GET request received")
 		handleGokuGet(w, r)
 
 	// all other cases
 	default:
+		logger.Info().Msg("Goku other request received")
 		handleGokuDefault(w, r)
 	}
 
@@ -36,26 +51,50 @@ func handleGokuRequests(w http.ResponseWriter, r *http.Request) {
 func handleGokuPost(w http.ResponseWriter, r *http.Request) {
 	// Reads the body of the JSON request into the fighter struct
 	// by unmarshaling the JSON bytes
-	f := fighter{}
+	f := fighters{}
+	logger.Info().Msg("Reading POST request...")
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to read POST request body.")
 	}
-	json.Unmarshal(body, &f)
+	logger.Info().Msg("Unmarshaling POST request...")
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to unmarshal POST request body.")
+	}
 
-	// Concatinates the name from the POST body with the string
-	// then marshals the response back into JSON bytes to
-	// send to the browser.
-	a := allegiance{
-		Message: f.Name + " has joined the Z squad",
+	// The struct that will hold the JSON message that is sent back to the browser
+	a := allegiance{}
+
+	// Checks for any JSON name and power fields that are empty, or zero.
+	// (Not that important so this could be removed, I just prefer names to not be "" and powers to not be 0)
+	for _, fighter := range f.Fighters {
+		if fighter.Name == "" {
+			logger.Error().Msg("Some fighter name JSON fields are empty.\n")
+			a.Message = "Some fighter name JSON fields are empty. Check the console log and the README."
+			return
+		}
+		if fighter.Power == 0 {
+			logger.Error().Msg("Some fighter power JSON fields are 0.\n")
+			a.Message = "Some fighter power JSON fields are 0. Check the console log and the README."
+			return
+		}
 	}
+
+	// The gokuPOSTCases function evaluates how many fighters are in the JSON POST body
+	// and returns a concatinated string with the name of the fighter(s) and other information
+	a.Message = gokuPOSTCases(f)
+
+	// The response is marshaled back into JSON bytes to be sent to the browser
+	logger.Info().Msg("Marshaling POST message...")
 	d, err := json.Marshal(a)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to marshal POST message.")
 	}
 
 	// Writes the JSON bytes to the JSON response writer
 	w.Write(d)
+	logger.Info().Msg("POST message written\n")
 }
 
 func handleGokuGet(w http.ResponseWriter, r *http.Request) {
@@ -63,13 +102,15 @@ func handleGokuGet(w http.ResponseWriter, r *http.Request) {
 	a := allegiance{
 		Message: "The Z fighters are ready to battle",
 	}
+	logger.Info().Msg("Marshaling GET message...")
 	d, err := json.Marshal(a)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to marshal GET message.")
 	}
 
 	// Writes the JSON bytes to the JSON response writer
 	w.Write(d)
+	logger.Info().Msg("GET message written\n")
 }
 
 func handleGokuDefault(w http.ResponseWriter, r *http.Request) {
@@ -77,11 +118,13 @@ func handleGokuDefault(w http.ResponseWriter, r *http.Request) {
 	a := allegiance{
 		Message: "The Z fighters perished",
 	}
+	logger.Info().Msg("Marshaling message...")
 	d, err := json.Marshal(a)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("Failed to marshal message.")
 	}
 
 	// Writes the JSON bytes to the JSON response writer
 	w.Write(d)
+	logger.Info().Msg("Message written\n")
 }
