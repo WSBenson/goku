fad36d04955d6b9828e42dddda82f0ab198aa570
Willem Benson <willemb17@gmail.com>
2020-06-10 16:51:21 -0400

    updated to ES 7.6.2 and put mapping in /etc

--- cmd/es.go
+++ cmd/es.go
@@ -41,7 +41,7 @@ func init() {
 	rootCmd.AddCommand(elasticCmd)
 	// flag to
 	elasticCmd.Flags().StringP("es_address", "a", "localhost:9200", "url for Elasticsearch server")
-	elasticCmd.Flags().StringP("es_mapping_file", "m", "./mapping.json", "location for es mapping file")
+	elasticCmd.Flags().StringP("es_mapping_file", "m", "./etc/mapping.json", "location for es mapping file")
 	// binds the port key to the pflag with viper
 	viper.BindPFlag("es_address", elasticCmd.Flags().Lookup("es_address"))
 	viper.BindPFlag("es_mapping_file", elasticCmd.Flags().Lookup("es_mapping_file"))
